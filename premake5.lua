67c0b7443e0f74c84fc7081de50ff7e20cb2a1d6
Ljr <3220101751@zju.edu.cn>
2025-03-31 12:00:40 +0800

    S007:premake配置

---
+++ premake5.lua
@@ -0,0 +1,113 @@
+workspace "Violet"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Violet"
+	location "Violet"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines
+		{
+			"VL_PLATFORM_WINDOWS",
+			"VL_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox" )
+		}
+
+	filter "configurations:Debug"
+		defines "VL_DEBUG"
+		symbols "On"
+		buildoptions "/utf-8"
+
+	filter "configurations:Debug"
+		defines "VL_RElEASE"
+		optimize "On"
+		buildoptions "/utf-8"
+
+
+	filter "configurations:Debug"
+		defines "VL_DIST"
+		optimize "On"
+		buildoptions "/utf-8"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Violet/vendor/spdlog/include",
+		"Violet/src"
+	}
+
+	links
+	{
+		"Violet"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.22621.0"
+
+		defines
+		{
+			"VL_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "VL_DEBUG"
+		symbols "On"
+		buildoptions "/utf-8"
+
+	filter "configurations:Debug"
+		defines "VL_RElEASE"
+		optimize "On"
+		buildoptions "/utf-8"
+
+
+	filter "configurations:Debug"
+		defines "VL_DIST"
+		optimize "On"
+		buildoptions "/utf-8"
