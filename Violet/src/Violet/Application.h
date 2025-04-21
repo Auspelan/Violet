50717dcf443b59fa7b41cafa79ebb58763155813
Ljr <3220101751@zju.edu.cn>
2025-04-21 11:45:31 +0800

    S012:窗口事件

--- Violet/src/Violet/Application.h
+++ Violet/src/Violet/Application.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Violet {
@@ -13,7 +15,11 @@ namespace Violet {
 
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
